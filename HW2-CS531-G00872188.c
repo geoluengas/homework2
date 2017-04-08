ea84d88b28d9bf8b66be3893b57f340e676b7970
geoluengas <geoluengas@gmail.com>
2017-04-08 19:15:37 -0400

    Added Erik's name to file. Added typedef for sighandler added DEBUG preprocessor def Added handler for SIGQUIT Changed stdin,stdout, stderr to names defined in unistd.h use perror for error reporting correctly fenced DEBUG statements

--- HW2-CS531-G00872188.c
+++ HW2-CS531-G00872188.c
@@ -1,9 +1,11 @@
 /*
  * File:   HW2-CS531-G00872188.c
  *
+ * Yobani Luengas G00872188
+ * Erik Arvidson G00379567
+ *
  * Created on March 21, 2017, 7:57 PM
  */
-
 
 #include <signal.h>
 #include <stdio.h>
@@ -13,6 +15,13 @@
 #include <unistd.h>
 #include <string.h>
 #include <limits.h>
+#include <errno.h>
+
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+
+typedef void (*sighandler_t)(int);
 
 /**
  * 
@@ -35,32 +44,38 @@ int main(int argc, char *argv[]) {
 
 int cs531_system(const char *comm) {
 
-    int original_handler; //stores the original signal handler before being changed by the program
-    int temp_handler; //stores the temporary signal handler set by the program
+    if (strlen(&comm) == 0) {
+        fprintf(stderr, "Error: no command provided. Exiting.\n");
+        return -1;
+    }
+
+    sighandler_t int_handler; //stores the original SIGINT signal handler before being changed by the program
+    sighandler_t quit_handler; //stores the original SIGQUIT signal handler before being changed by the program
 
     int child_status; //child exit status
     pid_t child_pid; //child process ID
 
-    if (!comm) { //Check if argument string is null. Return -1 if comm==null
-        fprintf(stderr, "Error: Command is %s.\n", comm);
+    //Ignore SIGINT while child is running
+    if ((int_handler = signal(SIGINT, handle_signal)) == SIG_ERR) {
+        perror("signal");
         return -1;
     }
 
-    //Ignore SIGINT while child is running
-    if ((original_handler = signal(SIGINT, handle_signal)) == SIG_ERR) {
-        fprintf(stderr, "signal not caught.");
+    //Ignore SIGQUIT while child is running
+    if ((quit_handler = signal(SIGQUIT, handle_signal)) == SIG_ERR) {
+        perror("signal");
+        return -1;
     }
 
     pid_t pid = fork(); //Fork child process to run execvp()
 
-    if (pid == NULL) {//if child process, run execvp
-
+    if (pid == (pid_t) 0) {//if child process, run execvp
         int tt = open("/dev/tty", O_RDWR); //open file descriptor for terminal
-        close(0); //close stdin
+        close(STDIN_FILENO); //close stdin
         dup(tt); //duplicate terminal to stdin
-        close(1); //close stdout
+        close(STDOUT_FILENO); //close stdout
         dup(tt); //duplicate terminal to stdout
-        close(2); //close stdterr
+        close(STDERR_FILENO); //close stderr
         dup(tt); //duplicate terminal to stderr
         close(tt); //close the tty
 
@@ -68,21 +83,24 @@ int cs531_system(const char *comm) {
         execlp("/bin/sh", "/bin/sh", "-c", comm, NULL);
 
         //If execlp fails, print an error
-        fprintf(stderr, "Error: Unsupported command or command failed to execute.\n");
+        perror("execlp");
         return -1;
 
     } else {//if parent process
-        if (pid == (pid_t) (-1)) { //error out if fork() failed
-            fprintf(stderr, "Error: Fork failed.\n");
+        if (pid == (pid_t) - 1) { //error out if fork() failed
+            perror("fork");
             return -1;
         } else {
             //wait for child proc to complete
             child_pid = wait(&child_status);
 
             //Restore SIGINT handler to previous state
-            temp_handler = signal(SIGINT, original_handler);
+            signal(SIGINT, int_handler);
+            signal(SIGQUIT, quit_handler);
 
-            printf("DEBUG{ Parent(%ld): Child(%ld) exited with status = %d }\n", (long) getpid(), (long) child_pid, child_status);
+#if DEBUG
+            printf("Parent(%ld): Child(%ld) exited with status = %d\n", (long) getpid(), (long) child_pid, child_status);
+#endif
         }
     }
     return child_status; // return exit status of child process
@@ -91,7 +109,14 @@ int cs531_system(const char *comm) {
 void handle_signal(int signal) {
     switch (signal) {
         case SIGINT:
-            fprintf(stderr, "DEBUG{ Caught SIGINT for PID :%d }\n", getpid());
+#if DEBUG
+            fprintf(stderr, "Caught SIGINT for PID :%d\n", getpid());
+#endif
+            break;
+        case SIGQUIT:
+#if DEBUG
+            fprintf(stderr, "Caught SIGQUIT for PID :%d\n", getpid());
+#endif
             break;
         default:
             fprintf(stderr, "Signal not handled : %d", signal);
